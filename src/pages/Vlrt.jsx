5f2d3da38df0ed88e066153ece9de68c60a465c5
Chaeyoung Shin <scy0723123@gmail.com>
2024-10-13 20:03:54 +0900

    #4 feat:미입력값 경고 모달

--- src/pages/Vlrt.jsx
+++ src/pages/Vlrt.jsx
@@ -7,9 +7,9 @@ const players = Array.from({ length: 10 }, (_, index) => `Player ${index + 1}`);
 export default function Vlrt() {
   const [playerData, setPlayerData] = useState(
     players.map((_, index) => ({
-      playerName: `Player ${index + 1}`,
-      tier: "Iron",
-      pts: 7,
+      playerName: "",
+      tier: "",
+      pts: 0,
     })),
   );
 
@@ -33,6 +33,7 @@ export default function Vlrt() {
   });
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isWarningModalOpen, setIsWarningModalOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
@@ -46,14 +47,54 @@ export default function Vlrt() {
     const updatedPlayers = [...playerData];
     updatedPlayers[index][field] = value;
     if (field === "tier") {
-      updatedPlayers[index].pts = tierToPoints[value];
+      updatedPlayers[index].pts = tierToPoints[value] || 0;
     }
     setPlayerData(updatedPlayers);
   };
 
-  const handleGenerateTeams = () => {
-    const sortedPlayers = [...playerData].sort((a, b) => b.pts - a.pts);
+  const handleGenerateSpinner = (players) => {
+    const spinner = document.createElement("div");
+    spinner.className =
+      "fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50";
+    spinner.innerHTML = `
+      <div class="animate-spin rounded-full h-24 w-24 border-t-4 border-b-4 bg-transparent border-purple-800 mb-4"></div>
+      <span class="text-white text-3xl text-yellow-300">Generating...</span>
+    `;
+    const container = document.querySelector(".vlrt__container");
+    container.appendChild(spinner);
 
+    setTimeout(() => {
+      spinner.remove();
+      generateTeams(players);
+    }, 500);
+  };
+
+  const handleGenerateTeams = () => {
+    const isAnyFieldEmpty = playerData.some(
+      (player) => !player.playerName || !player.tier,
+    );
+
+    if (isAnyFieldEmpty) {
+      setIsWarningModalOpen(true);
+    } else {
+      handleGenerateSpinner(playerData);
+    }
+  };
+
+  const handleContinueWithDefaults = () => {
+    const updatedPlayers = playerData.map((player, index) => ({
+      playerName: player.playerName || `Player ${index + 1}`,
+      tier: player.tier || "Iron",
+      pts: tierToPoints[player.tier || "Iron"],
+    }));
+
+    setPlayerData(updatedPlayers);
+    setIsWarningModalOpen(false);
+    handleGenerateSpinner(updatedPlayers);
+  };
+
+  const generateTeams = (players) => {
+    const sortedPlayers = [...players].sort((a, b) => b.pts - a.pts);
     let team1 = [];
     let team2 = [];
     let team1Pts = 0;
@@ -70,30 +111,20 @@ export default function Vlrt() {
     });
 
     setTeams({ team1, team1Pts, team2, team2Pts });
-
-    const spinner = document.createElement("div");
-    spinner.className =
-      "fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50";
-    spinner.innerHTML = `
-      <div class="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-800 mb-4 bg-transparent"></div>
-      <span class="text-white text-3xl text-yellow-300 bg-transparent">Generating...</span>
-    `;
-    const container = document.querySelector(".vlrt__container");
-    container.appendChild(spinner);
-
-    setTimeout(() => {
-      spinner.remove();
-      setIsModalOpen(true);
-    }, 500);
+    setIsModalOpen(true);
   };
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
 
+  const handleCloseWarningModal = () => {
+    setIsWarningModalOpen(false);
+  };
+
   if (isMobile) {
     return (
-      <div className="flex items-center justify-center min-h-screen text-white bg-gray-900">
+      <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
         <h1 className="text-3xl">GB는 모바일 기기에서 지원되지 않습니다.</h1>
       </div>
     );
@@ -111,7 +142,7 @@ export default function Vlrt() {
         />
       ))}
 
-      <div className="flex justify-center mt-4 bg-transparent">
+      <div className="mt-4 flex justify-center bg-transparent">
         <button
           className="mt-[20px] flex w-[300px] items-center justify-center rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 px-4 py-2 text-[30px] text-white shadow-sm hover:from-purple-700 hover:to-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           type="submit"
@@ -126,6 +157,30 @@ export default function Vlrt() {
         teams={teams}
         onClose={handleCloseModal}
       />
+
+      {isWarningModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="rounded-md p-10 text-center">
+            <h2 className="do-hyeon-regular mb-10 text-4xl">
+              Please fill out all fields.
+            </h2>
+            <div className="flex justify-center gap-5">
+              <button
+                onClick={handleContinueWithDefaults}
+                className="do-hyeon-regular rounded-md bg-red-500 px-7 py-3 text-2xl text-white"
+              >
+                Generate anyway
+              </button>
+              <button
+                onClick={handleCloseWarningModal}
+                className="do-hyeon-regular rounded-md bg-green-500 px-7 py-3 text-2xl text-white"
+              >
+                OK
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
