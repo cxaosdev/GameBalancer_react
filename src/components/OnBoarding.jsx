285b8871035bb65bc29997a52496b4d9b3c2c0d1
Chaeyoung Shin <scy0723123@gmail.com>
2024-10-26 23:05:28 +0900

    #18 온보딩 css 수정 및 이미지 교체

--- src/components/OnBoarding.jsx
+++ src/components/OnBoarding.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import SelectGame from "../assets/ScreenShots/SelectGame.png";
-import SelectTier from "../assets/ScreenShots/SelectTiers.png";
+import selectGame from "../assets/ScreenShots/SelectGame.png";
+import selectTier from "../assets/ScreenShots/SelectTiers.png";
 
 export default function Onboarding() {
   const navigate = useNavigate();
@@ -11,26 +11,29 @@ export default function Onboarding() {
   };
 
   return (
-    <div className="mt-[10rem] flex min-h-screen w-[40rem] items-center justify-center rounded-xl bg-black bg-opacity-70 p-0">
+    <div className="mt-[10rem] flex min-h-screen w-[45rem] items-center justify-center rounded-xl bg-black bg-opacity-70 p-0">
       <div className="mb-8">
-        <ol className="do-hyeon-regular mx-auto my-[3rem] flex max-w-md list-inside list-decimal flex-wrap text-left">
+        <ol className="do-hyeon-regular mx-auto my-[5rem] flex max-w-md list-inside list-decimal flex-wrap gap-y-10 text-left">
           <li className="mb-3 text-2xl">
             Select your game.
-            <img className="my-[1rem]" src={SelectGame} alt="" />
+            <img className="my-[1rem]" src={selectGame} alt="" />
           </li>
           <li className="mb-3 text-2xl">
             Enter players' ranks.<br></br>
-            <span className="text-xl">
+            <span className="text-lg text-gray-400">
               ✔︎ Players without a specified tier are considered to be at the
               lowest tier for calculation purposes.
             </span>
-            <img className="my-[1rem]" src={SelectTier} alt="" />
+            <img className="my-[1rem]" src={selectTier} alt="" />
           </li>
-          <li className="mb-3 text-2xl">
+          <li className="mb-3 flex flex-col text-2xl">
             Click 'Generate Fair Match!' to see the balanced match results.
-            <button className="New-Amsterdam ml-2 mt-4 flex w-[18rem] cursor-default items-center justify-center rounded-md bg-gradient-to-r from-purple-600 to-indigo-600 px-4 py-2 text-[30px] text-white shadow-sm">
-              <span>Generate Fair Match!</span>
-            </button>
+            <div className="w[100%] flex justify-center">
+              {" "}
+              <button className="New-Amsterdam ml-2 mt-10 flex w-[18rem] cursor-default items-center justify-center rounded-md bg-gradient-to-r from-purple-600 to-indigo-600 px-4 py-2 text-[30px] text-white shadow-sm">
+                <span>Generate Fair Match!</span>
+              </button>
+            </div>
           </li>
         </ol>
       </div>
